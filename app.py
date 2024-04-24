edf8027ba0e7e0a8b6f027bd5431e2a9763c8700
Yash Magoo <ymagoo@yamaha-motor-india.com>
2024-04-24 12:56:22 +0530

    image returned as base64

--- app.py
+++ app.py
@@ -4,14 +4,21 @@ from dotenv import load_dotenv
 import boto3
 import io
 from uuid import uuid4  # For generating unique filenames
+import requests
+import time
+from flask_cors import CORS
+import base64
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
 ACCESS_KEY=os.getenv('ACCESS_KEY')
 SECRET_KEY=os.getenv('SECRET_KEY')
 BUCKET_NAME=os.getenv('BUCKET_NAME')
+CLOUDFRONT_URL=os.getenv('CLOUDFRONT_URL')
+FOLDER_NAME=os.getenv('FOLDER_NAME')
 
 @app.route('/', methods=['GET'])
 def home():
@@ -29,18 +36,28 @@ def upload_image():
     filename_with_location = f"ValidationImages/{filename}"
 
     s3_client = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+
     try:
         s3_client.upload_fileobj(image_file, BUCKET_NAME, filename_with_location)
 
-        cdn = 'https://d1z61jdqkglv9s.cloudfront.net'
-        url = cdn + '/OutputImages' + '/processed_image_' + filename
+        url = CLOUDFRONT_URL + FOLDER_NAME + '/processed_image_' + filename
 
-        return jsonify({'url': url}), 200
+        # Try to fetch the image 30 times
+        for _ in range(30):
+            try:
+                response = requests.get(url, verify=False)
+                response.raise_for_status()  # If the response contains an HTTP error status code, raise an exception
+                base64_content = base64.b64encode(response.content).decode('utf-8')
+                print(base64_content)
+                return jsonify({'image': base64_content}), 200  # Return the image content
+            except Exception as e:
+                time.sleep(2)  # Wait for 2 seconds before the next attempt
+
+        # If all attempts fail, return an error message
+        return jsonify({'error': 'Not able to process file'}), 500
     except Exception as e:
         return jsonify({'error': f'Error uploading image: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5001)
+    app.run(debug=True,host='0.0.0.0',port=5000)
 
-
-ougujrrm
