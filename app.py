7b9e0a3870d042bfac52d2c96b4dfabcc1a98cd3
Yash Magoo <ymagoo@yamaha-motor-india.com>
2024-04-29 12:35:01 +0530

    upload_zip function completed

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ from flask import Flask,request,jsonify
 import os
 from dotenv import load_dotenv
 import boto3
-import io
-from uuid import uuid4  # For generating unique filenames
-import requests
-import time
+from uuid import uuid4
+
 from flask_cors import CORS
 import base64
-from utils import fetch_files_from_zip_variable
+from utils import fetch_files_from_zip_variable, save_input_file_into_bucket, fetch_output_file_from_bucket
 from pathlib import Path
 from pathlib import Path
 
@@ -27,9 +25,9 @@ BUCKET_NAME=os.getenv('BUCKET_NAME')
 CLOUDFRONT_URL=os.getenv('CLOUDFRONT_URL')
 FOLDER_NAME=os.getenv('FOLDER_NAME')
 
-# @app.route('/', methods=['GET'])
-# def home():
-#     return 'Hello World!!!'
+
+
+
 
 # Function to upload image to S3 bucket
 @app.route('/upload', methods=['POST'])
@@ -40,30 +38,14 @@ def upload_image():
     image_file = request.files['image']
     filename = f"{uuid4()}.{image_file.filename.split('.')[-1]}"
 
-    filename_with_location = f"ValidationImages/{filename}"
-
-    s3_client = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
-
     try:
-        s3_client.upload_fileobj(image_file, BUCKET_NAME, filename_with_location)
+        save_input_file_into_bucket(s3_client, image_file, filename)
+        base64_output_image = fetch_output_file_from_bucket(filename)
 
-        url = CLOUDFRONT_URL + FOLDER_NAME + '/processed_image_' + filename
-
-        # Try to fetch the image 30 times
-        for _ in range(30):
-            try:
-                response = requests.get(url, verify=False)
-                response.raise_for_status()  # If the response contains an HTTP error status code, raise an exception
-                base64_content = base64.b64encode(response.content).decode('utf-8')
-                print(base64_content)
-                return jsonify({'image': base64_content}), 200  # Return the image content
-            except Exception as e:
-                time.sleep(2)  # Wait for 2 seconds before the next attempt
-
-        # If all attempts fail, return an error message
-        return jsonify({'error': 'Not able to process file'}), 500
+        return jsonify({'image': base64_output_image})
     except Exception as e:
-        return jsonify({'error': f'Error uploading image: {str(e)}'}), 500
+        return jsonify({'error': 'Error uploading image'}), 500
+    
 
 
 @app.route('/upload_zip', methods=['POST'])
@@ -77,42 +59,36 @@ def upload_zip():
         return jsonify({'error': 'No selected file'})
 
     if file and file.filename.endswith('.zip'):
-
         uuid = str(uuid4())
-        file_name = uuid + file.filename
-        file_name = file_name.replace(" ", "_")
+        file_name = file.filename #secure_filename(file.filename)
         file.save(os.path.join('temp_files',file_name))
         path = os.path.join('temp_files',file_name)
         saved_file_path = os.path.join('temp_files',uuid)
 
         with ZipFile(path, 'r') as zip: 
-            # printing all the contents of the zip file 
-            zip.printdir() 
-            # extracting all the files 
-            print('Extracting all the files now...') 
             zip.extractall(saved_file_path) 
-            print('Done!') 
 
+        input_images_path = os.path.join(saved_file_path, file_name.split('.')[0])
 
-        return jsonify({'message': 'File uploaded successfully'})
+        # processed_images = []
+        processed_images = {}
+
+        for filename in os.listdir(input_images_path):
+            file_path = os.path.join(input_images_path, filename)
+            with open(file_path, 'rb') as f:
+                filename_for_s3 = uuid + filename
+                save_input_file_into_bucket(s3_client, f, filename_for_s3)
+            processed_image_base64 = fetch_output_file_from_bucket(filename_for_s3)
+            # processed_image_with_filename = {filename.split('.')[0]: processed_image_base64}
+            # processed_images.append(processed_image_with_filename)
+            processed_images[filename.split('.')[0]] = processed_image_base64
+        return jsonify({'images': processed_images})
     else:
         return jsonify({'error': 'Invalid file format. Please upload a .zip file'})
-    
 
 
 
 if __name__ == '__main__':
     Path("temp_files").mkdir(parents=True, exist_ok=True)
+    s3_client = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
     app.run(debug=True,host='0.0.0.0',port=5000)
-
-    # if 'zip' not in request.files:
-    #     return jsonify({'error': 'No zip file uploaded'}), 400
-
-    # zip_file = request.files['zip']
-    # zip_data = zip_file.read()
-
-    # file_contents = fetch_files_from_zip_variable(zip_data)
-
-    # zip_data.printdir()
-
-    # return jsonify(file_contents), 200
--- utils.py
+++ utils.py
@@ -1,6 +1,14 @@
 from zipfile import ZipFile
 import io
 import os
+import requests
+from flask import jsonify
+import base64
+import time
+
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+CLOUDFRONT_URL = os.getenv('CLOUDFRONT_URL')
+FOLDER_NAME=os.getenv('FOLDER_NAME')
 
 def fetch_files_from_zip_variable(zip_data):
     # Create a file-like object from the zip data
@@ -22,3 +30,24 @@ def fetch_files_from_zip_variable(zip_data):
                 file_contents[file] = f.read()
 
     return file_contents
+
+def save_input_file_into_bucket(s3_client, image_file, filename):
+        filename_with_s3_location = f"ValidationImages/{filename}"
+
+        s3_client.upload_fileobj(image_file, BUCKET_NAME, filename_with_s3_location)
+
+# returns file in base64 format
+def fetch_output_file_from_bucket(filename):
+    url = CLOUDFRONT_URL + FOLDER_NAME + '/processed_image_' + filename
+
+    # Try to fetch the image 30 times
+    for _ in range(30):
+        try:
+            response = requests.get(url, verify=False)
+            response.raise_for_status()  # If the response contains an HTTP error status code, raise an exception
+            base64_content = base64.b64encode(response.content).decode('utf-8')
+            print(base64_content)
+            return base64_content
+        except:
+            time.sleep(2)  # Wait for 2 seconds before the next attempt
+    raise Exception()
